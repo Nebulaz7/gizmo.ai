c3ff084558ef11dd974290d3e2be9cecead321d8
Peters Joshua <127890151+Nebulaz7@users.noreply.github.com>
2024-12-28 04:58:05 +0100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// config.js
+const config = {
+  apiKey: btoa('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').split('').reverse().join('')
+};
+
+export default config;
